7c52d3f7b8b5eecd66069df775d6cac229d8cab3
guganravi06 <105877037+guganravi06@users.noreply.github.com>
2024-01-28 10:28:17 +0530

    Update server.js
    modified server.js file to include cors.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const twilio = require('twilio');
+const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors()); 
 
 // Replace with your Twilio credentials
 const accountSid = 'ACea985c54e346bc24e68483e1a7f80cb4';
